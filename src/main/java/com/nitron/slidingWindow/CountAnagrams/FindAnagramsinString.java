dfb4c57722c11e4c6bfecc4ce95f33f888206c9c
nishit <nishitmv@gmail.com>
2022-05-24 00:46:35 +0530

    Find Anagrams

---
+++ src/main/java/com/nitron/slidingWindow/CountAnagrams/FindAnagramsinString.java
@@ -0,0 +1,59 @@
+package com.nitron.slidingWindow.CountAnagrams;
+
+import java.util.*;
+
+public class FindAnagramsinString {
+    public List<Integer> findAnagrams(String str, String pattern) {
+
+        int n = str.length();
+        int k = pattern.length();
+        char[] subStr = new char[k];
+        StringBuilder tmp = new StringBuilder();
+        char[] input = str.toCharArray();
+        Map<Character, Integer> map = buildCharCountMap(pattern);
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; ) {
+
+            if (i == 0) {
+                int j = 0;
+                while (j < k) {
+                    subStr[j] = input[j];
+                    j++;
+                    i++;
+                }
+                tmp = new StringBuilder(String.valueOf(subStr));
+            } else {
+                StringBuilder builder = new StringBuilder();
+                tmp = builder.append(tmp.substring(1)).append(input[i]);
+                i++;
+            }
+
+            if (isAnagram(tmp, map)) res.add(i-k);
+        }
+
+        return res;
+    }
+
+    private Map<Character, Integer> buildCharCountMap(String pattern) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (Character ch : pattern.toCharArray()) {
+            if (map.containsKey(ch)) map.put(ch, map.get(ch) + 1);
+            else map.put(ch, 1);
+        }
+        return map;
+    }
+
+    private boolean isAnagram(StringBuilder tmp, Map<Character, Integer> patternMap) {
+
+        Map<Character, Integer> tmpMap = buildCharCountMap(tmp.toString());
+
+        for(Character ch : tmpMap.keySet())
+        {
+            if(!patternMap.containsKey(ch) ||  (!Objects.equals(patternMap.get(ch), tmpMap.get(ch))))
+                return false;
+        }
+    return true;
+    }
+}
--- src/main/java/com/nitron/slidingWindow/CountAnagrams/Main.java
+++ src/main/java/com/nitron/slidingWindow/CountAnagrams/Main.java
@@ -4,10 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String str = "rtkjprepggxrpnrvystmwcysyycqpevikeffmznimkkas";
-        String pattern = "vwsrenzk";
+        String str = "cbaebabacd";
+        String pattern = "abc";
 
         System.out.println(new CountAnagramsinStringOptimized().countAnagrams(str, pattern));
+
+        System.out.println(new FindAnagramsinString().findAnagrams(str, pattern));
 
     }
 }
