f93f71f6287c2e9256e0313c75acc870b5722a1c
nishit <nishitmv@gmail.com>
2022-05-26 14:07:21 +0530

    Longest SubString with No Repeating Characters

---
+++ src/main/java/com/nitron/slidingWindow/LongestSubstrNoRepeatingChars/Main.java
@@ -0,0 +1,12 @@
+package com.nitron.slidingWindow.LongestSubstrNoRepeatingChars;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String str  = "abcabcbb";
+
+        System.out.println(new LongestSubStrWithNoRepeatingChars().calc(str));
+    }
+
+}
