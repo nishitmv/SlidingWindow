12d89955869cf5cf91273d5c8b801bb609f804a7
nishit <nishitmv@gmail.com>
2022-05-26 14:11:11 +0530

    Longest SubString with No Repeating Characters

--- src/main/java/com/nitron/slidingWindow/LongestSubstrNoRepeatingChars/LongestSubStrWithNoRepeatingChars.java
+++ src/main/java/com/nitron/slidingWindow/LongestSubstrNoRepeatingChars/LongestSubStrWithNoRepeatingChars.java
@@ -7,47 +7,42 @@ public class LongestSubStrWithNoRepeatingChars {
     public int calc(String str) {
 
         int n = str.length();
-        if(n ==1)
+        if (n == 1)
             return 1;
-        if(n == 0)
+        if (n == 0)
             return 0;
-        int i =0;
-        int j=0;
+        int i = 0;
+        int j = 0;
         int result = 0;
         Map<Character, Integer> map = new HashMap<>();
 
-        while(j<n)
-        {
-            while(j<n) {
+        while (j < n) {
+            while (j < n) {
                 char key = str.charAt(j);
                 if (!map.containsKey(key)) {
                     map.put(key, 1);
                     result = Math.max(result, map.size());
                     j++;
                 } else {
-
                     map.compute(key, (k, v) -> v + 1);
-
                     j++;
                     break;
                 }
             }
 
-            while(i<n) {
+            while (i < n) {
                 char keyAti = str.charAt(i);
                 if (map.get(keyAti) == 1) {
                     map.remove(keyAti);
                     i++;
-                }
-                else if(map.get(keyAti)>1) {
+                } else if (map.get(keyAti) > 1) {
                     map.compute(keyAti, (k, v) -> v - 1);
                     i++;
                     break;
                 }
-
             }
         }
 
-    return result;
+        return result;
     }
 }
